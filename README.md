04669276a7c3178057a8a18707d5d63501e3bf11
ErikNixdorf <erik.nixdorf@ufz.de>
2020-08-17 15:25:08 +0200

    script to function

--- README.md
+++ README.md
@@ -1,21 +1,35 @@
 # radocorrect
 
-coming soon
+Radocorrect is a tool which corrects radolan precipitation estimates from DWD using the ground-truth measurements at the rain gauges. The **aim** is to retrieve hourly gridded precipitation information which e.g. can be applied in hydrological models.
 
-## Features
+## Approach
+1.  the algorithm computes a weighting factor for each time step at the rain gauge locations by comparing radolan and gauge estimates.
+2. The weighting factors are distributed spatially on the radolan grid using inverse distance weighting
+3. At each radolan grid cell, the radolan precipitation estimate is multiplied with the corresponding weighting factor
+4. Spatio-temporal datasets are stacked as xarray dataset which can be exported to netcdf files
 
-
-## Dependencies
-
-* Python 3
-...
+##Dependencies
+* roverweb 
+* nrt_io
+* for others, see requirements.txt
 
 ## Limitations
+* IDW Interpolation only
+* No in-depth analysis of deviation between radolan and gauge depending on rain pattern structure (e.g duration, intensity of event)
+* Outlier (very high weighting factors) can remove by setting percentile threshold, only
+* Domain boundarys have to be provided as shapefiles
 
 ## Code Example
 ```python
+import radocorrect
+radocorrect.radocorrect(start_time='2007-11-05:1500',
+              end_time='2007-11-06:1800',
+              domain_path='.\\Input\\Mueglitz_basin_grid.shp',
+              dwd_search_area_path='.\\Input\\dwd_rectangle.shp',
+              no_of_nearest_stations=5,
+              thresh_percentile=0.95,
+              output=True)
 ```
-
 ## Authors
 
 * **Erik Nixdorf**
--- radocorrect.py
+++ radocorrect.py
@@ -2,7 +2,7 @@
 This tool aims to correct the radolan data with the data from the DWD Stations
 10 min time shift has to be accepted as error
 author: Erik Nixdorf
-Version: 0.1
+Version: 0.2
 Legal rights belong to: UFZ & Erik Nixdorf
 
 """
@@ -71,133 +71,142 @@ def Interpolation_2D(dataarray,method='cubic'):
                               (xx, yy),
                                  method=method)
     return interpolated_array    
-#%% setting
-start_time='2007-11-05:1500'
-end_time='2007-11-06:1800'
-domain_path='.\\Input\\Mueglitz_basin_grid.shp'
-dwd_search_area_path='.\\Input\\dwd_rectangle.shp'
-no_of_nearest_stations=5
-#define a relative threshold (percentile) to remove outliers in the correction factor
-thresh_percentile=0.95
-
-#%% Get the radolan data for a certain time period
-
-rado_init=downloader.downloader(start_time=start_time, end_time=end_time,roi=dwd_search_area_path,roi_buffer=0.01)
-
-rado_rain_raw=rado_init.radorecent(time_res='hourly',to_harddisk=True)
-
-rado_rain=10*rado_rain_raw.RW # to mm
-# make invalid values to nan
-rado_rain=rado_rain.where(rado_rain>=0,drop=True)
-
-#manipulate the time by 10 min
-rado_rain.coords['time'].values=rado_rain.coords['time'].values+np.timedelta64(10,'m')
-#get rado coordinates
-rado_x=rado_rain.coords['x'].values
-rado_y=rado_rain.coords['y'].values
-#create empty array which will be for the factor between station and radolan data
-rain_multiplicator = xr.DataArray(np.empty([rado_rain.sizes['time'],rado_rain.sizes['y'],rado_rain.sizes['x']])*np.nan,
-                    dims=rado_rain.dims,
-                    coords=rado_rain.coords)
+#%% 
+def radocorrect(start_time='2007-11-05:1500',
+              end_time='2007-11-06:1800',
+              domain_path='.\\Input\\Mueglitz_basin_grid.shp',
+              dwd_search_area_path='.\\Input\\dwd_rectangle.shp',
+              no_of_nearest_stations=5,
+              thresh_percentile=0.95,
+              output=True):
+    """
+    The main function
+    a relative threshold (percentile) to remove outliers in the correction factor
+    """
 
 
+    #%% Get the radolan data for a certain time period
 
-#%%get the data from dwd stations
-#import the grid
-domain=gpd.GeoDataFrame.from_file(domain_path)
-#find nearest stations
-test,dwd_base=weather.Find_nearest_dwd_stations(domain,
-    date_start=datetime.strptime(start_time,'%Y-%m-%d:%H%M').date().strftime('%Y%m%d'),
-    date_end=datetime.strptime(end_time,'%Y-%m-%d:%H%M').date().strftime('%Y%m%d'),
-    data_category='precipitation',
-    temp_resolution='hourly',
-    no_of_nearest_stations=no_of_nearest_stations,
-    memory_save=True,
-    Output='True')
+    rado_init=downloader.downloader(start_time=start_time, end_time=end_time,roi=dwd_search_area_path,roi_buffer=0.01)
 
-#cut the dwd database to the actual dates of the radolan dataset
-dwd_base=dwd_base.sel(time=rado_rain.coords['time'].values)
-print(str(len(dwd_base.STATIONS_ID)),'Stations with valid rainfall data used')
-#%%load the station network  and check their positions on radogrid
-#check file
-files=os.listdir('.\\roverweb\\tables\\')
-dwd_station_file= [file for file in files if '.csv' in file][0]
-dwd_stations=pd.read_csv('.\\roverweb\\tables\\'+dwd_station_file)
-#select rows based on dwd data
-rel_station_list=list(np.array(dwd_base.STATIONS_ID))
-dwd_stations=dwd_stations.loc[dwd_stations['STATIONS_ID'].isin(rel_station_list)]
-dwd_stations['geo_x']=np.nan
-dwd_stations['geo_y']=np.nan
-#transform the coordinates to radolan
-#define the radolan_projection
-rado_proj_string='+proj=stere +lat_0=90 +lat_ts=90 +lon_0=10 +k=0.93301270189 + x_0=0 +y_0=0 +a=6370040 +b=6370040 +to_meter=1000 +no_defs'
-rado_proj = CRS(rado_proj_string)
-#define a transformer to deal with the differenct CRS
-transformer = Transformer.from_crs(CRS('epsg:4326'), rado_proj, always_xy=True)
-#convert the bounds str to radolan projection
-dwd_lonlat=dwd_stations[['geo_lon','geo_lat']]
-dwd_stations[['geo_x','geo_y']]=np.array([transformer.transform(*xy) for xy in np.array(dwd_stations[['geo_lon','geo_lat']])]) 
+    rado_rain_raw=rado_init.radorecent(time_res='hourly',to_harddisk=False)
 
-#%%find the columns and rows in radolan
+    rado_rain=10*rado_rain_raw.RW # to mm
+    # make invalid values to nan
+    rado_rain=rado_rain.where(rado_rain>=0,drop=True)
 
-position_indices=np.array(dwd_stations.apply(get_nearest_indices,axis=1,geometry_column=['geo_x','geo_y'],coordinate_array=[rado_x,rado_y]).to_list())
-# add station ID as last column
-position_indices=np.append(position_indices,np.array(dwd_stations.STATIONS_ID).reshape(np.size(position_indices,0),1),axis=1)
-#next we loop with isel through the entire rado_rain to get hour data
-for station in position_indices:
-    station_rado=rado_rain.isel(x=station[0],y=station[1])
-    #replace zero with 0.01 to avoid 0/0 division
-    station_rado=station_rado.where(station_rado!=0,other=0.01)
-    #same for dwd data
-    station_measured=dwd_base.sel(STATIONS_ID=station[2]).hourly_precipitation_height
-    station_measured=station_measured.where(station_measured!=0,other=0.01)
-    #get the multiplicator for each time step
-    station_multiplicator=station_measured/station_rado
-    #replace the entry in the multiplicator dataarray, remember that variable order is time, y, x
-    rain_multiplicator[:,station[1],station[0]]=station_multiplicator
-    
-
-#%% Next we interpolate over the nans using a loop, apply is possible as well
-for time_step in range(0,rain_multiplicator.sizes['time']):
-    #check whether at least for the minimum number of stations exist non-nan values
-    #because the problem is that radolan has invalid data nan
-    if np.count_nonzero(~np.isnan(rain_multiplicator[time_step,:,:]))<no_of_nearest_stations:
-        print('Not enough valid points to interpolated for time step ',str(time_step))
-    else:   
-        interpolated_grid=Interpolation_2D(rain_multiplicator[time_step,:,:],method='linear')
-        # we have to interpolate a second time to extrapolate all remaining nans, which are outside convex hull
-        # discussion @ https://stackoverflow.com/questions/21993655/interpolation-and-extrapolation-of-randomly-scattered-data-to-uniform-grid-in-3d
-        nearest_grid=Interpolation_2D(rain_multiplicator[time_step,:,:],method='nearest')
-        #replace the nan of initial interpolation with the ones from nearest grid
-        nan_locations=np.isnan(interpolated_grid)
-        interpolated_grid[nan_locations]=nearest_grid[nan_locations]
-        rain_multiplicator[time_step,:,:]=interpolated_grid
-    
-#%% reduce data array dimension to actual roi coordinates
-#get domain bounds
-domain_loader=downloader.downloader(start_time=start_time, end_time=end_time,roi=domain_path,roi_buffer=0.01)
-domain_bounds_rado=np.array([transformer.transform(*xy) for xy in domain_loader.roi_bounds])
-
-rain_multiplicator=rain_multiplicator.sel(x=slice(domain_bounds_rado[0,0],domain_bounds_rado[1,0]),
-                                          y=slice(domain_bounds_rado[0,1],domain_bounds_rado[1,1])
-                                          )
-rado_rain=rado_rain.sel(x=slice(domain_bounds_rado[0,0],domain_bounds_rado[1,0]),
-                                          y=slice(domain_bounds_rado[0,1],domain_bounds_rado[1,1])
-                                          )
-
-#%% multiply datasets and append to original xarray
-#set all values above threshold to threshold
-threshold=rain_multiplicator.quantile(thresh_percentile)
-rain_multiplicator=rain_multiplicator.where(rain_multiplicator<threshold,threshold)
-rado_corrected=rado_rain[:,:,:]*rain_multiplicator[:,:,:]
-#%% write final dataset
-ds_out = rado_rain.to_dataset(name = 'radolan')
-ds_out['correction_factor']=rain_multiplicator
-ds_out['radolan_corrected']=rado_corrected.where(rado_corrected>=0)
-#sort the time
-ds_out=ds_out.sortby('time')
-#write it out
-os.makedirs("Output", exist_ok=True) 
-ds_out.to_netcdf('.\\Output\\radolan_correct_'+start_time[0:10]+'_'+end_time[0:10]+'.nc')
+    #manipulate the time by 10 min
+    rado_rain.coords['time'].values=rado_rain.coords['time'].values+np.timedelta64(10,'m')
+    #get rado coordinates
+    rado_x=rado_rain.coords['x'].values
+    rado_y=rado_rain.coords['y'].values
+    #create empty array which will be for the factor between station and radolan data
+    rain_multiplicator = xr.DataArray(np.empty([rado_rain.sizes['time'],rado_rain.sizes['y'],rado_rain.sizes['x']])*np.nan,
+                        dims=rado_rain.dims,
+                        coords=rado_rain.coords)
 
 
+
+    #%%get the data from dwd stations
+    #import the grid
+    domain=gpd.GeoDataFrame.from_file(domain_path)
+    #find nearest stations
+    test,dwd_base=weather.Find_nearest_dwd_stations(domain,
+        date_start=datetime.strptime(start_time,'%Y-%m-%d:%H%M').date().strftime('%Y%m%d'),
+        date_end=datetime.strptime(end_time,'%Y-%m-%d:%H%M').date().strftime('%Y%m%d'),
+        data_category='precipitation',
+        temp_resolution='hourly',
+        no_of_nearest_stations=no_of_nearest_stations,
+        memory_save=True,
+        Output='True')
+
+    #cut the dwd database to the actual dates of the radolan dataset
+    dwd_base=dwd_base.sel(time=rado_rain.coords['time'].values)
+    print(str(len(dwd_base.STATIONS_ID)),'Stations with valid rainfall data used')
+    #%%load the station network  and check their positions on radogrid
+    #check file
+    files=os.listdir('.\\roverweb\\tables\\')
+    dwd_station_file= [file for file in files if '.csv' in file][0]
+    dwd_stations=pd.read_csv('.\\roverweb\\tables\\'+dwd_station_file)
+    #select rows based on dwd data
+    rel_station_list=list(np.array(dwd_base.STATIONS_ID))
+    dwd_stations=dwd_stations.loc[dwd_stations['STATIONS_ID'].isin(rel_station_list)]
+    dwd_stations['geo_x']=np.nan
+    dwd_stations['geo_y']=np.nan
+    #transform the coordinates to radolan
+    #define the radolan_projection
+    rado_proj_string='+proj=stere +lat_0=90 +lat_ts=90 +lon_0=10 +k=0.93301270189 + x_0=0 +y_0=0 +a=6370040 +b=6370040 +to_meter=1000 +no_defs'
+    rado_proj = CRS(rado_proj_string)
+    #define a transformer to deal with the differenct CRS
+    transformer = Transformer.from_crs(CRS('epsg:4326'), rado_proj, always_xy=True)
+    #convert the bounds str to radolan projection
+    dwd_lonlat=dwd_stations[['geo_lon','geo_lat']]
+    dwd_stations[['geo_x','geo_y']]=np.array([transformer.transform(*xy) for xy in np.array(dwd_stations[['geo_lon','geo_lat']])]) 
+
+    #%%find the columns and rows in radolan
+
+    position_indices=np.array(dwd_stations.apply(get_nearest_indices,axis=1,geometry_column=['geo_x','geo_y'],coordinate_array=[rado_x,rado_y]).to_list())
+    # add station ID as last column
+    position_indices=np.append(position_indices,np.array(dwd_stations.STATIONS_ID).reshape(np.size(position_indices,0),1),axis=1)
+    #next we loop with isel through the entire rado_rain to get hour data
+    for station in position_indices:
+        station_rado=rado_rain.isel(x=station[0],y=station[1])
+        #replace zero with 0.01 to avoid 0/0 division
+        station_rado=station_rado.where(station_rado!=0,other=0.01)
+        #same for dwd data
+        station_measured=dwd_base.sel(STATIONS_ID=station[2]).hourly_precipitation_height
+        station_measured=station_measured.where(station_measured!=0,other=0.01)
+        #get the multiplicator for each time step
+        station_multiplicator=station_measured/station_rado
+        #replace the entry in the multiplicator dataarray, remember that variable order is time, y, x
+        rain_multiplicator[:,station[1],station[0]]=station_multiplicator
+        
+
+    #%% Next we interpolate over the nans using a loop, apply is possible as well
+    for time_step in range(0,rain_multiplicator.sizes['time']):
+        #check whether at least for the minimum number of stations exist non-nan values
+        #because the problem is that radolan has invalid data nan
+        if np.count_nonzero(~np.isnan(rain_multiplicator[time_step,:,:]))<no_of_nearest_stations:
+            print('Not enough valid points to interpolated for time step ',str(time_step))
+        else:   
+            interpolated_grid=Interpolation_2D(rain_multiplicator[time_step,:,:],method='linear')
+            # we have to interpolate a second time to extrapolate all remaining nans, which are outside convex hull
+            # discussion @ https://stackoverflow.com/questions/21993655/interpolation-and-extrapolation-of-randomly-scattered-data-to-uniform-grid-in-3d
+            nearest_grid=Interpolation_2D(rain_multiplicator[time_step,:,:],method='nearest')
+            #replace the nan of initial interpolation with the ones from nearest grid
+            nan_locations=np.isnan(interpolated_grid)
+            interpolated_grid[nan_locations]=nearest_grid[nan_locations]
+            rain_multiplicator[time_step,:,:]=interpolated_grid
+        
+    #%% reduce data array dimension to actual roi coordinates
+    #get domain bounds
+    domain_loader=downloader.downloader(start_time=start_time, end_time=end_time,roi=domain_path,roi_buffer=0.01)
+    domain_bounds_rado=np.array([transformer.transform(*xy) for xy in domain_loader.roi_bounds])
+
+    rain_multiplicator=rain_multiplicator.sel(x=slice(domain_bounds_rado[0,0],domain_bounds_rado[1,0]),
+                                              y=slice(domain_bounds_rado[0,1],domain_bounds_rado[1,1])
+                                              )
+    rado_rain=rado_rain.sel(x=slice(domain_bounds_rado[0,0],domain_bounds_rado[1,0]),
+                                              y=slice(domain_bounds_rado[0,1],domain_bounds_rado[1,1])
+                                              )
+
+    #%% multiply datasets and append to original xarray
+    #set all values above threshold to threshold
+    threshold=rain_multiplicator.quantile(thresh_percentile)
+    rain_multiplicator=rain_multiplicator.where(rain_multiplicator<threshold,threshold)
+    rado_corrected=rado_rain[:,:,:]*rain_multiplicator[:,:,:]
+    #%% write final dataset
+    ds_out = rado_rain.to_dataset(name = 'radolan')
+    ds_out['correction_factor']=rain_multiplicator
+    ds_out['radolan_corrected']=rado_corrected.where(rado_corrected>=0)
+    #sort the time
+    ds_out=ds_out.sortby('time')
+    #write it out
+    if output:
+        os.makedirs("Output", exist_ok=True) 
+        ds_out.to_netcdf('.\\Output\\radolan_correct_'+start_time[0:10]+'_'+end_time[0:10]+'.nc')
+    return ds_out
+
+#%% Test our functions
+if __name__ == "__main__":
+    radocorrect()
